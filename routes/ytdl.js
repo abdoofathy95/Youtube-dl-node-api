838b60b3dffcc669649c723ce1df0aca6da807bb
abdoofathy <abdoofathy95@gmail.com>
2018-08-26 13:31:01 +0200

    added cookie for remote fetching

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
-var youtubedl = require('youtube-dl');
+const express = require('express');
+const router = express.Router();
+const youtubedl = require('youtube-dl');
+const fs = require('fs');
+const uuid = require('uuidv4');
 
 /* GET users listing. */
 router.get('/version', function(req, res, next) {
@@ -10,11 +12,16 @@ router.get('/version', function(req, res, next) {
 router.get('/videoInfo', function(req, res, next) {
   const url = req.query.url;
   const userAgent = req.headers['user-agent'];
+  const fileLocation = `cookies_${uuid()}.txt`;
 
-  const options = ['--no-playlist', '--skip-download', '--print-json', `--user-agent=${userAgent}`];
+  const options = ['--no-playlist', '--skip-download', '--print-json', `--user-agent=${userAgent}`, `--cookies=${fileLocation}`, ];
   youtubedl.getInfo(url, options, function(err, info){
     if(err) res.send(err);
-    res.send(info);
+    fs.readFile(fileLocation, 'utf8', function(err, contents) {
+      if(err) res.send(err);
+      res.json({info: info, cookie: contents});
+    });
+    fs.unlinkSync(fileLocation);
   });
 })
 
