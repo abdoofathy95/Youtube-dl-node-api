f617ac8bb6eb65dff64e0a546b130a3db5c57193
abdoofathy <abdoofathy95@gmail.com>
2018-08-26 14:40:39 +0200

    clean up

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const youtubedl = require('youtube-dl');
-const fs = require('fs');
-const uuid = require('uuidv4');
 
 /* GET users listing. */
 router.get('/version', function(req, res, next) {
@@ -14,14 +12,10 @@ router.get('/videoInfo', function(req, res, next) {
   const userAgent = req.headers['user-agent'];
   const fileLocation = `cookies_${uuid()}.txt`;
 
-  const options = ['--no-playlist', '--skip-download', '--print-json', `--user-agent=${userAgent}`, `--cookies=${fileLocation}`, ];
+  const options = ['--no-playlist', '--skip-download', '--print-json', `--user-agent=${userAgent}`, ];
   youtubedl.getInfo(url, options, function(err, info){
     if(err) res.send(err);
-    // fs.readFile(fileLocation, 'utf8', function(err, contents) {
-      // if(err) res.send(err);
       res.json({info: info, cookie: contents});
-    // });
-    // fs.unlinkSync(fileLocation);
   });
 })
 
